799974d21ce01e571a98676899249f14546699a0
Víctor Loras <71260643+Loracio@users.noreply.github.com>
2021-03-21 16:53:56 +0100

    Más ejemplos y pequeños errrores

--- RetratoDeFases.py
+++ RetratoDeFases.py
@@ -5,7 +5,7 @@ class RetratoDeFases2D:
     """
     Hace un retrato de fases de un sistema 2D.
     """
-    def __init__(self, dF, RangoRepresentacion, LongitudMalla, *, Densidad = 1, Polar = False, Titulo = 'Retrato de Fases', xlabel = 'X', ylabel = 'Y'):
+    def __init__(self, dF, RangoRepresentacion, LongitudMalla, *, Densidad = 1, PrimerCuadrante = False, Polar = False, Titulo = 'Retrato de Fases', xlabel = 'X', ylabel = r"$\dot{X}$"):
         """
         Inicializador de clase: inicializa las variables de la clase a los valores pasados. 
         También se definen las variables que se emplean internamente en la clase para realizar el diagrama.
@@ -18,17 +18,21 @@ class RetratoDeFases2D:
         self.L = LongitudMalla              # Número de puntos por eje para representar el diagrama (habrá L² puntos)
 
         # Variables no obligatorias
-        self.Densidad = Densidad            # [RetratoDeFases.py](RetratoDeFases.py)
-        self.Polar = Polar                  # Si se pasan las coordenadas en polares, marcar como True.
-        self.Titulo = Titulo                # Titulo para el retrato de fases.
-        self.xlabel = xlabel                # Titulo en eje X
-        self.ylabel = ylabel                # Titulo en eje Y
+        self.Densidad = Densidad                # Controla la cercanía de las líneas de flujo
+        self.PrimerCuadrante = PrimerCuadrante  # Indica si sólo se debe plotear el primer cuadrante
+        self.Polar = Polar                      # Si se pasan las coordenadas en polares, marcar como True.
+        self.Titulo = Titulo                    # Titulo para el retrato de fases.
+        self.xlabel = xlabel                    # Titulo en eje X
+        self.ylabel = ylabel                    # Titulo en eje Y
 
 
         # Variables que el usuario no debe emplear: son para el tratamiento interno de la clase. Es por ello que llevan el prefijo "_"
-        self._X, self._Y = np.meshgrid(np.linspace(-self.Rango, self.Rango, self.L), np.linspace(-self.Rango, self.Rango, self.L))   #Crea una malla de tamaño L²
+        if self.PrimerCuadrante:
+            self._X, self._Y = np.meshgrid(np.linspace(0, self.Rango, self.L), np.linspace(0, self.Rango, self.L))   #Crea una malla de tamaño L² empezando en el cero
+        else:
+            self._X, self._Y = np.meshgrid(np.linspace(-self.Rango, self.Rango, self.L), np.linspace(-self.Rango, self.Rango, self.L))   #Crea una malla de tamaño L²
 
-        if Polar:   
+        if self.Polar:   
             self._R, self._Theta = (self._X**2 + self._Y**2)**0.5, np.arctan2(self._Y, self._X) # Transformacion de coordenadas cartesianas a polares
             self._dR, self._dTheta = self.dF(self._R, self._Theta) # Calcula el campo de velocidades en cada uno de los puntos de la malla
             self._dX, self._dY = self._dR*np.cos(self._Theta) - self._R*np.sin(self._Theta)*self._dTheta, self._dR*np.sin(self._Theta)+self._R*np.cos(self._Theta)*self._dTheta
@@ -40,7 +44,10 @@ class RetratoDeFases2D:
         colorines = (self._dX**2+self._dY**2)**(0.5)
         plt.streamplot(self._X, self._Y, self._dX, self._dY, color=colorines, linewidth=1, density= self.Densidad)
         plt.axis('square')
-        plt.axis([-self.Rango, self.Rango, -self.Rango, self.Rango])
+        if self.PrimerCuadrante:
+            plt.axis([0, self.Rango, 0, self.Rango])
+        else:
+            plt.axis([-self.Rango, self.Rango, -self.Rango, self.Rango])
         plt.title(f'{self.Titulo}')
         plt.xlabel(f'{self.xlabel}')
         plt.ylabel(f'{self.ylabel}')
--- ejemplos.py
+++ ejemplos.py
@@ -18,13 +18,13 @@ if __name__ == '__main__':
     RetratoOscilador.plot()
 
     """
-    Ejemplo de uso #2: retrato de fases de un sistema en coordenadas polares (Example 6.3.2 Strogatz)
+    Ejemplo de uso #2: retrato de fases de un sistema en coordenadas polares (Example 7.1.1 Strogatz)
     """
 
     def dFPolares(r, theta):
-        return 0.5*r*(1 - r*r), 1+0*theta # Si se juega con este último valor salen cosas interesantes...
+        return 0.5*r*(1 - r*r), 1+0*theta
 
-    RetratoPolares = RetratoDeFases2D(dFPolares, 3, 30, Polar= True, Titulo= 'Retrato en coord. polares (Example 6.3.2)')
+    RetratoPolares = RetratoDeFases2D(dFPolares, 3, 30, Polar=True, Titulo='Ciclo Límite')
     RetratoPolares.plot()
 
     """
@@ -40,7 +40,86 @@ if __name__ == '__main__':
     RetratoLoveAffairs.plot()
 
     """
-    Ejemplo de uso #4: péndulo simple
+    Ejemplo de uso #4: Rabbits vs Sheep
+    """
+    def dFRabbitsVsSheep(x, y):
+        return x*(3-x-2*y), y*(2-x-y)
+
+    RetratoRabbitsVsSheep = RetratoDeFases2D(dFRabbitsVsSheep, 3, 300, PrimerCuadrante=True, Densidad=2, Titulo='Rabbits vs Sheep', xlabel='Rabbits', ylabel='Sheep')
+    RetratoRabbitsVsSheep.plot()
+
+    """
+    Ejemplo de uso #5: Centro no lineal
+    """
+    def dFCentroNoLineal(x, y):
+        return y-y**3, -x-y**2
+
+    RetratoCentroNoLineal = RetratoDeFases2D(dFCentroNoLineal, 2, 2000, Densidad=2, Titulo='Centro no lineal en (0,0)', xlabel='X', ylabel='Y')
+    RetratoCentroNoLineal.plot()
+
+    """
+    Ejemplo de uso #6: Exercise 6.1.8 van der Pol oscillator
+    """
+    def dFVanDerPol(x, y):
+        return y, -x + y*(1-x**2)
+
+    RetratoVanDerPol = RetratoDeFases2D(dFVanDerPol, 6, 2000, Densidad=1.5, Titulo='Oscilador de van der Pol', xlabel='X', ylabel='Y')
+    RetratoVanDerPol.plot()
+
+    """
+    Ejemplo de uso #7: Exercise 6.1.9 Dipole fixed point
+    """
+    def dFDipolo(x, y):
+        return 2*x*y, y*y-x*x
+
+    RetratoDipolo = RetratoDeFases2D(dFDipolo, 10, 2000, Densidad=1.5, Titulo='Dipolo', xlabel='X', ylabel='Y')
+    RetratoDipolo.plot()
+
+    """
+    Ejemplo de uso #8: Exercise 6.1.10 Two-Eyed Monster
+    """
+    def dFMonster(x, y):
+        return y + y*y, -0.5*x + 0.20*y - x*y + 1.20*y*y
+
+    RetratoMonster = RetratoDeFases2D(dFMonster, 10, 2000, Densidad=1.5, Titulo='Monster', xlabel='X', ylabel='Y')
+    RetratoMonster.plot()
+
+    """
+    Ejemplo de uso #9: Exercise 6.1.11 Parrot
+    """
+    def dFParrot(x, y):
+        return y + y*y, -x + 0.20*y - x*y + 1.20*y*y
+
+    RetratoParrot = RetratoDeFases2D(dFParrot, 10, 2000, Densidad=1.5, Titulo='Parrot', xlabel='X', ylabel='Y')
+    RetratoParrot.plot()
+
+    """
+    Ejemplo de uso #10: Exercise 6.6.3 Wallpaper
+    """
+    def dFWallpaper(x, y):
+        return np.sin(y), np.sin(x)
+
+    RetratoWallpaper = RetratoDeFases2D(dFWallpaper, 7.5, 2000, Densidad=2, Titulo='Wallpaper', xlabel='X', ylabel='Y')
+    RetratoWallpaper.plot()
+
+    """
+    Ejemplo de uso #11: Exercise 6.6.4 Computer Explorations
+    """
+
+    def dFCompEx2(x, y):
+        return y - y**3, x*np.cos(y)
+
+    RetratoCompEx2 = RetratoDeFases2D(dFCompEx2, 3, 300, Densidad=2, Titulo='Computer Explorations #2')
+    RetratoCompEx2.plot()
+
+    def dFCompEx3(x, y):
+        return np.sin(y), y**2 - x
+
+    RetratoCompEx3 = RetratoDeFases2D(dFCompEx2, 3, 300, Densidad=2, Titulo='Computer Explorations #3')
+    RetratoCompEx3.plot()
+
+    """
+    Ejemplo de uso #12: Péndulo simple
     """
 
     def dFPendulo(theta, v):
@@ -50,7 +129,7 @@ if __name__ == '__main__':
     RetratoPendulo.plot()
 
     """
-    Ejemplo de uso #5: péndulo simple amortiguado
+    Ejemplo de uso #13: Péndulo simple amortiguado
     """
 
     def dFPenduloAmortiguado(theta, v):
